c0dc2930e2659b485acc3af469786db63ab813a3
Subramanian Thayuman <subramanianthayuman@gmail.com>
2022-06-28 15:14:52 +0530

    Portfolio project uploaded

---
+++ Class Work/JS/23.06.2022/index.js
@@ -0,0 +1,70 @@
+//23.06.2022
+
+//let :
+//1. we can Declare the Variable Name in one line and initilaize on another line.
+//2. The variables with same name aren't possible with let.
+//3. We can reassign/update the value of the variable.
+//4. let is block scpoed.
+//5. let dosen't supports hoisting (Hoisting allows functions to be safely used in code before they are declared)
+
+let myName;
+console.log(myName);
+myName = "Mani";
+console.log(myName);
+myName = "Bruce";
+console.log(myName);
+
+console.log("--------let End--------")
+
+//const :
+//1. Must declare and initialize variable in the same line itself.
+//2. The variables with same name aren't possible with const.
+//3. We can't reassign/update the value of the variable.
+//4. const is block scpoed.
+//5. const dosen't supports hoisting (Hoisting allows functions to be safely used in code before they are declared)
+
+const myAge = 15;
+console.log(myAge);
+
+console.log("--------const End--------")
+
+//var :
+//1. we can Declare the Variable Name in one line and initilaize on another line.
+//2. The variables with same name are possible with var.
+//3. We can reassign/update the value of the variable.
+//4. let is global scpoed.
+//5. var supports hoisting (Hoisting allows functions to be safely used in code before they are declared)
+
+var myCountry;
+myCountry = "India";
+console.log(myCountry);
+myCountry = "Russia";
+console.log(myCountry)
+
+var myCountry = "Antartica";
+console.log(myCountry);
+
+console.log("--------var End--------")
+
+//Functions are Hoisted. Because of that we can access the function even before declaring them.
+addition()
+console.log(addition) // Returns function definition
+function addition(){
+    var result = 4 * 5;
+    console.log(result)
+}
+
+console.log("--------function End--------")
+
+//undefined - When we try to acess a variable that is declared but not initialized.
+//not defined - When we try to acess a variable that is not declared.
+//null - Its a value that we can initialize to a variable which will later be replaced with some other value.
+
+let myPlace = null;
+console.log(myPlace);
+myPlace = "Trichy";
+console.log(myPlace);
+
+//console.llog(myArea)  //Example for not defined
+
+console.log("--------The End--------")
---
+++ Class Work/JS/24.06.2022/index.js
@@ -0,0 +1,47 @@
+//24.06.2022
+
+//OPERATORS
+
+//Arithmetic Operators
+console.log(10 + 5);    //15
+console.log(10 - 5);    //5
+console.log(10 * 5);    //50
+console.log(10 / 5);    //2
+console.log(10 % 5);    //0
+
+console.log("End Aritmetic Operators")
+
+//Comparision Operators
+console.log(2 < 3);     //true
+console.log(2 > 3);     //false
+console.log(2 <= 3);     //true
+console.log(2 >= 3);     //false
+
+// == (double equals) or != and === (triple equals) or !==
+
+//double equals or != check only the values and not the data type.
+//triple equals or !== check both the values and the data type.
+
+console.log('== and ===')
+
+console.log(3 == '3')   //true
+console.log(3 === '3')   //false
+console.log(3 === 3 )   //true
+console.log(3 === 3)   //true
+
+console.log(3 != '3')   //false
+console.log(3 !== '3')  //true
+console.log(3 !== 3 )   //false
+console.log(3 !== 3)   //false
+console.log('raja' !== '5') //true
+console.log('raja' !== 'raja') //false
+console.log(1 !== '1')  //true
+
+console.log("End of Comparision Operators")
+
+//Bitwise Operators
+
+// & (And), | (OR)
+
+console.log(2 & 3);
+console.log(2 | 3);
